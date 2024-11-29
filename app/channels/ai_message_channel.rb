fd9cffa6cc5170e96ff586f4273007d927a16d4c
Daniel Arenas <danielarenas@lahaus.com>
2024-11-29 17:03:35 -0500

    fix channel conection

--- app/channels/ai_message_channel.rb
+++ app/channels/ai_message_channel.rb
@@ -3,7 +3,7 @@ class AiMessageChannel < ApplicationCable::Channel
     assistant_id = params[:assistant_id]
     thread_id    = params[:thread_id]
 
-    assistant = Assistant.find(params[:assistant_id])
+    assistant = Assistant.find_by_code(params[:assistant_id])
     messages = assistant.messages.find_by_thread_id(thread_id).messages
 
     stream_from "ai_message_channel_#{thread_id}"
