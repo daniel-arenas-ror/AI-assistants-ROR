645d5689cbc49e7a47d2e48eea75ef48e90068a6
Daniel Arenas <danielarenas@lahaus.com>
2024-11-29 16:11:44 -0500

    fix assistance show

--- app/controllers/v1/assistants_controller.rb
+++ app/controllers/v1/assistants_controller.rb
@@ -1,19 +1,11 @@
 module V1
   class AssistantsController < ApplicationController
     def show
-      assistant = Assistant.find_by_code(params[:assistant_id])
+      assistant = Assistant.find_by_code(params[:id])
 
       render json: {
         assistant: assistant
       }, status: :accepted
-    end
-
-    private
-
-    def thread_params
-      params.permit(
-        :assistant_id
-      )
     end
   end
 end
